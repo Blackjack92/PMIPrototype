988b70775f28b6b4d8672ebcbff2e227fbc8bb6d
Kevin Wallis <kevin.wallis@hotmail.com>
2017-05-06 17:53:44 +0200

    Added logic to read key pairs.

--- PMIUtils/tests/SerializationTest.java
+++ PMIUtils/tests/SerializationTest.java
@@ -1,11 +1,16 @@
+import com.serialization.KeyPairReader;
 import com.serialization.ObjectDeserializer;
 import com.serialization.ObjectSerializer;
 import com.serialization.SimpleCertificate;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
@@ -31,10 +36,33 @@ public class SerializationTest {
             assertEquals(cert2.getPublicKey(), deserializedCert2.getPublicKey());
             assertEquals(cert2.getSerialNumber(), deserializedCert2.getSerialNumber());
 
-        } catch (IOException e) {
-            fail(e.getMessage());
-        } catch (ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
             fail(e.getMessage());
         }
     }
+
+    @Test
+    void readKeyPair() throws Exception {
+        String privateFileName = "./out/test/PMIUtils/keys/private_key.der";
+        String publicFileName = "./out/test/PMIUtils/keys/public_key.der";
+
+        PrivateKey privateKey = KeyPairReader.readPrivateKey(privateFileName);
+        assertNotNull(privateKey);
+
+        PublicKey publicKey = KeyPairReader.readPublicKey(publicFileName);
+        assertNotNull(publicKey);
+
+        KeyPair keyPair = KeyPairReader.readKeyPair(publicFileName, privateFileName);
+        assertNotNull(keyPair);
+
+        assertEquals(privateKey.getAlgorithm(), keyPair.getPrivate().getAlgorithm());
+        assertEquals(privateKey.getFormat(), keyPair.getPrivate().getFormat());
+
+        assertEquals(publicKey.getAlgorithm(), keyPair.getPublic().getAlgorithm());
+        assertEquals(publicKey.getFormat(), keyPair.getPublic().getFormat());
+
+        assertEquals(privateKey, keyPair.getPrivate());
+        assertEquals(publicKey, keyPair.getPublic());
+    }
+
 }
